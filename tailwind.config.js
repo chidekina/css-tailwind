6a736aac7f8722d6de4ee552e0d9296ddc524639
CESAR HIDEKI NAGANO <cesar0610@digitalcollegesul.local>
2024-09-11 12:01:12 -0300

    project initial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'digital': '#C81C6F'
+            },
+            spacing: {
+                '100': '100px',
+                '200' : '200px',
+            }
+        }
+    }
+}
